9310b268503572b8c6e33099ecc5350eee10d0f1
Gregory Dechant <dechantg@gmail.com>
2023-09-19 00:58:50 +0000

    First assignment for the lotide project

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed! " + actual + " === " + expected + " ✅✅✅");
+  } else {
+    console.log("⛔️⛔️⛔️ Assertion Failed! " + actual + " !== " + expected + " ⛔️⛔️⛔️");
+  }
+};
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Test String", "Test String");
+assertEqual(1, 2);

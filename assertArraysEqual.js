972cbb717b59f43d1e60a0340cdba8770d066b48
Gregory Dechant <dechantg@gmail.com>
2023-09-20 02:24:06 +0000

    Added the assertArraysEquals.js exercise to the folder

---
+++ assertArraysEqual.js
@@ -0,0 +1,35 @@
+
+
+
+const eqArrays = function(array1, array2) {
+  let result = false;
+  if (array1.length !== array2.length) {
+    result = false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      result = array1[i] === array2[i];
+    }
+  }
+  return result;
+
+}
+
+const assertArraysEqual = function(array1, array2) {
+  let result = true;
+  if (array1 !== array2) {
+    result = false;
+  } 
+  
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed! ${array1} === ${array2} ✅✅✅`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed! ${array1} !== ${array2} ⛔️⛔️⛔️`);
+  }
+
+}
+
+
+
+// TEST CODE
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
--- eqArrays.js
+++ eqArrays.js
@@ -25,8 +25,8 @@ const eqArrays = function(array1, array2) {
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false

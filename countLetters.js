402bbaaecdc103ad620cc6d1d080f24e7bb11a98
Gregory Dechant <dechantg@gmail.com>
2023-09-20 23:43:34 +0000

    adding the counting exercises

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed! ${actual} === ${expected} ✅✅✅`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed! ${actual} !== ${expected} ⛔️⛔️⛔️`);
+  }
+};
+
+
+const countLetters = function(string) {
+  const results = {};
+  for (const letter of string) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    } 
+  }
+
+  
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
+

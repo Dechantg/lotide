728d833cd3de182f0ca496b6280f3fb70216f358
Gregory Dechant <dechantg@gmail.com>
2023-09-23 23:41:41 +0000

    pushing my new version of findKeyByValues

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,35 +1,47 @@
+// redoing findKeyByValue function to make sure i understant principals behind it
 
 
+// copy assertEqual function into file
 
-const assertEqual = function(actual, expected) {
+const assertEqual = (actual, expected) => {
+
+  // create comparison for strings. If strings are absolutely equal then immedatly return a pass and exit the function.
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed! ${actual} === ${expected} ✅✅✅`);
-  } else {
-    console.log(`⛔️⛔️⛔️ Assertion Failed! ${actual} !== ${expected} ⛔️⛔️⛔️`);
+    return console.log(`✅✅✅ Assertion Passed! ${actual} === ${expected} ✅✅✅`);
   }
+
+  // if the condition above is not met then return to console a failure result
+  return console.log(`⛔️⛔️⛔️ Assertion Failed! ${actual} !== ${expected} ⛔️⛔️⛔️`);
+
 };
 
-const findKeyByValue = function(object, value) {
-  for (let key in object) {
-    if (object[key] === value) {
-      return key;
-    }
-  }
-  return undefined;
-};
+// test object
 
-// TEST CODE
-
-
-
-const bestTVShowsByGenre = {
+const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
+  drama:  "The Wire"
 };
 
-//console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-//console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));
+
+// start function to find a key by a value.
+
+const findKeyByValue = (object, value) => {
+
+ 
+  // loop the array to compare the keys against their values in the object
+
+  for (key in object) {
+    if (object[key] === value) {
+      // if comparison is true then return the key being observed
+      return key;
+    }     
+  }
+};
+
+// validate result with assertEqual
+
+
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

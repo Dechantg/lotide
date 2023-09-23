e27a3595a80da76368c3fe8723b139c8d4103aaa
Gregory Dechant <dechantg@gmail.com>
2023-09-23 22:15:21 +0000

    pushing my new version of countLetters

--- countLetters.js
+++ countLetters.js
@@ -1,29 +1,52 @@
+// recreating a count letters function in a fresh file then copy pasting over to make sure I understand the principles
 
 
-// const assertEqual = function(actual, expected) {
+// copy paste in my assertEqual for later validation
+
+// const assertEqual = (actual, expected) => {
+
+//   // create comparison for strings. If strings are absolutely equal then immedatly return a pass and exit the function.
 //   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed! ${actual} === ${expected} ✅✅✅`);
-//   } else {
-//     console.log(`⛔️⛔️⛔️ Assertion Failed! ${actual} !== ${expected} ⛔️⛔️⛔️`);
+//     return console.log(`✅✅✅ Assertion Passed! ${actual} === ${expected} ✅✅✅`);
 //   }
+
+//   // if the condition above is not met then return to console a failure result
+//   return console.log(`⛔️⛔️⛔️ Assertion Failed! ${actual} !== ${expected} ⛔️⛔️⛔️`);
+
 // };
 
+// create the function countLetters to analyse a string 
 
-const countLetters = function(string) {
-  const results = {};
-  for (const letter of string) {
-    if (letter !== " ") {
-      if (results[letter]) {
-        results[letter] += 1;
-      } else {
-        results[letter] = 1;
-      }
+const countLetters = (string) => {
+
+  //create an empty object to store results into
+  const result = {};
+    
+
+  // create a loop to filter for the results
+
+  for (let letters of string) {
+
+    // determin if the letter is already in the object, if so then pass on
+    if (letters in result) {
+      result[letters] += 1;
+    } else {
+
+      // if the letter is not already in the result, and does not equal a space, add it and go back to the loop
+      if (letters !== " ") {
+    result[letters] = 1
     }
   }
+  }
 
-  
-  return results;
+  // console.log the result then return it to the function
+
+  console.log(result);
+
+  return result
+
 };
 
-console.log(countLetters("lighthouse in the house"));
+countLetters("test string");
+
 

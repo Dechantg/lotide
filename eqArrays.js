2297ee581078a7d19ce3984811ba54e877b20293
Gregory Dechant <dechantg@gmail.com>
2023-09-23 21:17:00 +0000

    pushing my new version of eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,37 +1,51 @@
+// second attempt at eqArrays funtion to ensure I understand the principles of what was created. This was created from a seperate file then copy pasted back into lotide for upload. attempts to refactor and apply things learned later in the week have been incorporated
 
+// copy paste in the assertEqual function
 
-const assertEqual = function(actual, expected) {
+const assertEqual = (actual, expected) => {
+
+  // create comparison for strings. If strings are absolutely equal then immedatly return a pass and exit the function.
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed! ${actual} === ${expected} ✅✅✅`);
-  } else {
-    console.log(`⛔️⛔️⛔️ Assertion Failed! ${actual} !== ${expected} ⛔️⛔️⛔️`);
+    return console.log(`✅✅✅ Assertion Passed! ${actual} === ${expected} ✅✅✅`);
   }
-};
+
+  // if the condition above is not met then return to console a failure result
+  return console.log(`⛔️⛔️⛔️ Assertion Failed! ${actual} !== ${expected} ⛔️⛔️⛔️`);
+
+}
+
+// create the eqArray function
 
 const eqArrays = function(actual, expected) {
+
+  // compare array length. if lengths to not match immediatly return false
+
   if (actual.length !== expected.length) {
-    return false;
+          return false;
   }
-  
+
+  // create a loop to compare individual components in the arrays
+
   for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
       return false;
     }
-       
+   
   }
+
+  // if neither of the above conditions fail, return true
+  
   return true;
+  
 };
 
 
-// TEST CODE
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 
 
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1, 4]) // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
